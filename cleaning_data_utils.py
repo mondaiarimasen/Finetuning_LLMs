1964f90cef6cf5efa5f141f4b413bdeb7adf2fe3
Victor Zhang <victor.g.zhang@gmail.com>
2024-02-11 16:17:26 -0600

    new code for cleaning raw data

---
+++ cleaning_data_utils.py
@@ -0,0 +1,107 @@
+import re
+import os
+import sys
+from tqdm import tqdm
+from typing import List
+from datasets import load_dataset, Dataset
+
+
+# filtering out empty texts
+def filter_empty_texts(examples):
+    return [s for s in examples if s]
+
+# loading raw text and filtering
+def load_or_fetch_wikitext(dataset_name, dataset_config, dataset_path):
+  # Check if the dataset is already saved locally
+  if os.path.exists(dataset_path): # if it is
+    print(f"Loading dataset from {dataset_path}")
+    # Load the dataset from the file
+    dataset = load_dataset(dataset_name, dataset_config)
+    return dataset
+  else:
+    print(f"Fetching dataset from Hugging Face and caching it locally")
+    # Load the dataset from Hugging Face and cache it locally
+    dataset = load_dataset(dataset_name, dataset_config, cache_dir=dataset_path)
+    print("Dataset downloaded and saved.")
+    return dataset
+
+# check for emails in datasets, if true, sys.exit(); else can remove all @ in dataset in a different method
+def any_emails(dataset) -> bool:
+    print("\n checking for email addresses now")
+    email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+    email_addresses = {}
+    # Find all occurrences of the email pattern
+    for i in tqdm(range(len(dataset)), desc ="checking for emails in dataset"):
+        potential_emails = re.findall(email_pattern, dataset[i])
+        if potential_emails:
+            email_addresses[i] = potential_emails
+
+    # Check if any email addresses were found
+    if email_addresses:
+        print("Found email addresses:", email_addresses)
+        sys.exit()
+    else:
+        print("No email addresses found.")
+    return False
+
+# remove characters specified in the str chars_to_remove from text, returns cleaned text
+def remove_chars(chars_to_remove: str, text: str):
+
+    # Create a translation table mapping each character to None
+    translation_table = str.maketrans('', '', chars_to_remove)
+
+    # Remove the characters from the string
+    cleaned_text = text.translate(translation_table)
+    #print("\nthis is cleaned_text chars removed: ", cleaned_text)
+    return cleaned_text
+
+# removes whitespace inside " ", e.g. " hi " becomes "hi" in the text, returns cleaned text 
+def clean_quoted_whitespace(text):
+    # This pattern looks for a space after an opening quote or before a closing quote
+    pattern = r'" *([^"]*?) *"'
+    # This function will be used to replace the matched text
+    def replacer(match):
+        # This will replace the matched text with the text without leading/trailing spaces
+        return '"' + match.group(1) + '"'
+    # Replace all occurrences of the pattern with the result of the replacer function
+    cleaned = re.sub(pattern, replacer, text)
+    # Strip leading and trailing whitespace
+    return cleaned.strip()
+
+# removes whitespace in front of or behind specified strings, returns cleaned text
+def remove_custom(text: str, lst_to_remove_comp: List, lst_to_strip_pre_ws: List, lst_to_strip_post_ws: List) -> str:
+    for s in lst_to_remove_comp:
+        text = text.replace(s, "")
+
+    for s in lst_to_strip_pre_ws:
+        text = text.replace(" "+ s, s) 
+        
+    for s in lst_to_strip_post_ws:
+        text = text.replace(s+" ", s) 
+        
+    return text
+
+
+
+# cleans each string in given list of strings by removing specified chars, and string for which you want to remove preceding or post white space
+def clean_completely(dataset_list, chars_to_remove, lst_to_remove_comp, lst_to_strip_pre_ws, lst_to_strip_post_ws):
+    print("\nin clean_completely")
+    cleaned_lst = []
+    
+    print("removing empty strings")
+    dataset_list = filter_empty_texts(dataset_list)
+    print("finished removing empty strings")
+    
+    print("starting to clean each string in list")
+    for string in dataset_list:
+        text = remove_chars(chars_to_remove, string.strip())
+        text = clean_quoted_whitespace(text)
+        text = remove_custom(text, lst_to_remove_comp, lst_to_strip_pre_ws, lst_to_strip_post_ws)
+        
+        cleaned_lst.append(text.strip())
+
+    print("finished cleaning each string in list")
+    return cleaned_lst
+
+
--- saving_checking_filtered_text.py
+++ saving_checking_filtered_text.py
@@ -1,47 +1,14 @@
-import torch
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, AdamW, GPT2Config, get_linear_schedule_with_warmup, AutoTokenizer, TextDataset
-from datasets import load_dataset, Dataset
-import os
-import pandas as pd
-from tqdm import tqdm
-import wandb
-import psutil
-from datetime import datetime
-import pytz
-import json
-import sys
-import torch.nn.functional as func
-from typing import List
-import multiprocessing as mp
-import math
-# use dataloader for batching
-from torch.utils.data import DataLoader
+from cleaning_data_utils import *
 
 
 #### variables
+keys = ["test","validation", "train"]
+
 wandb_run_name = "training_loop"
 wandb_project = "gpt-2-finetuning"
 
 uni_delim = "\0" # a unique delimiter to save and load filtered text from dataset
 # Here we use "\0" (null character) as it's unlikely to be in the text
-
-####
-'''
-#### wandb setup
-
-wandb.login()
-# initialize WandB
-wandb.init(project=wandb_project, name= wandb_run_name)
-
-# using wanb to log ram usage
-def log_ram_usage():
-    # Get memory usage in percentage
-    memory_usage_percent = psutil.virtual_memory().percent
-    # Log the memory usage
-    wandb.log({"RAM Usage (%)": memory_usage_percent})
-
-#### finished wandb setup
-'''
 
 dataset_name = 'wikitext'
 dataset_config = 'wikitext-103-v1'
@@ -50,25 +17,10 @@ home_dir = os.getcwd()
 dataset_filename = '/wikitext-103-raw-v1'
 
 
-#### creating input_id and attention_masks tensors 
 
-# loading saved cleaned tokens for test, train, validation sets
 
-# loading raw text and filtering
-def load_or_fetch_wikitext(dataset_name, dataset_config, dataset_path):
-  # Check if the dataset is already saved locally
-  if os.path.exists(dataset_path): # if it is
-    print(f"Loading dataset from {dataset_path}")
-    # Load the dataset from the file
-    dataset = load_dataset(dataset_name, dataset_config)
-    return dataset
-  else:
-    print(f"Fetching dataset from Hugging Face and caching it locally")
-    # Load the dataset from Hugging Face and cache it locally
-    dataset = load_dataset(dataset_name, dataset_config, cache_dir=dataset_path)
-    print("Dataset downloaded and saved.")
-    return dataset
 
+#### loading saved cleaned tokens for test, train, validation sets
 
 dataset = load_or_fetch_wikitext(dataset_name, dataset_config, dataset_path_head + dataset_filename)
 
@@ -78,69 +30,92 @@ print(dataset)
 print(dataset['train'][:5])
 
 print(dataset['train'])
-print(dataset['train']['text'][:5])
-
-# filtering out empty texts
-def filter_empty_texts(examples):
-    return bool(examples['text'].strip())
-
-
-print("\nClean dataset does not exist and filtering empty text...")
-for key in dataset.keys():
-  print(f"Cleaning {key} dataset")
-  dataset[key] = dataset[key].filter(filter_empty_texts)
-  print(f"Finished cleaning {key} dataset")
-
-
-# check results
-print(f"\nChecking datasets")
-for split, ds in dataset.items():
-    print(f"{split} dataset: {len(ds)} rows after filtering")
 
 # viewing first few elements
 print("dataset['test']['text'][:5]: ", dataset['test']['text'][:5])
 # 
 
-# saving filtered text
+chars_to_remove = ""
+if any_emails(dataset['validation']['text']) == False:
+    # now removing selected chars
+    # Define the characters to be removed
+    chars_to_remove = "@="
+
+
+lst_to_remove_comp = [" / <unk> /" ," <unk> ", " <unk>", "<unk> "]
+lst_to_strip_pre_ws = [":", ";", ",", ".", "- ", "'", ")", "!", "]"]
+lst_to_strip_post_ws = ["-", "...", "(", "["]
+
+
+cleaned_dataset = {}
+
+for key in keys:
+    dataset_to_clean = dataset[key]['text']#[15:45]
+    cleaned_dataset[key] = {}
+    print(f"cleaning {key} dataset")
+
+    cleaned_dataset[key]['text'] = clean_completely(dataset_to_clean, chars_to_remove, lst_to_remove_comp, lst_to_strip_pre_ws, lst_to_strip_post_ws)
+    print(f"this is last ten ele of cleaned_dataset[{key}] after clean_completely:")
+    for item in cleaned_dataset[key]['text'][-20:]:
+        print(item)
+
+    #print("\n now printing original")
+    #for item in dataset_to_clean:
+    #    print(item)
+
+
+#print(clean_completely(["2000 - 2005"], chars_to_remove, lst_to_remove_comp, lst_to_strip_pre_ws, lst_to_strip_post_ws))
+#print("possible special character:", clean_completely(["2000 – 2005"], chars_to_remove, lst_to_remove_comp, lst_to_strip_pre_ws, lst_to_strip_post_ws))
+
+
+
+
+
+
+
+
+
+
+
+
+#### saving filtered text
 
 print("\n### Create the directory to save if it does not exist ###")
 # Create the directory if it does not exist
 directory = home_dir + "/" + dataset_config + "-filtered-text-lists"
 os.makedirs(directory, exist_ok=True)
 
-
-key = "validation"
-print("key: ", key)
-filename = "/" + key + "_text_filtered.txt"
-print("\n### saving to file ###")
-
-
-# Using a unique delimiter to join and save strings
-# Here we use uni_delim as it's unlikely to be in the text
-with open(directory + filename, 'w', encoding='utf-8') as file:
-    file.write(uni_delim.join(dataset[key]['text']))
-print("\n### finished saving to file ###")
-
-# 
-
-# loading filtered text 
-
-print("\n### loading from file ###")
-
-# Load the strings back, splitting by the unique delimiter
-with open(directory + filename, 'r', encoding='utf-8') as file:
-    loaded_filtered_text = file.read().split(uni_delim)
-
-print("\n### finished loading from file ###")
+for key in keys:
+    print("key: ", key)
+    filename = "/" + key + "_text_filtered.txt"
+    print("\n### saving to file ###")
 
 
-print("loaded_filtered_text[:5]: ", loaded_filtered_text[:5])
+    # Using a unique delimiter to join and save strings
+    # Here we use uni_delim as it's unlikely to be in the text
+    with open(directory + filename, 'w', encoding='utf-8') as file:
+        file.write(uni_delim.join(cleaned_dataset[key]['text']))
+    print("\n### finished saving to file ###")
 
-# checking if loaded filtered text matches filtered text computed
-if loaded_filtered_text == dataset[key]['text']:
-    print("The lists are the same.")
-else:
-    print("The lists are different.")
+
+    # loading filtered text to check integrity of saved file
+
+    print("\n### loading from file ###")
+
+    # Load the strings back, splitting by the unique delimiter
+    with open(directory + filename, 'r', encoding='utf-8') as file:
+        loaded_filtered_text = file.read().split(uni_delim)
+
+    print("\n### finished loading from file ###")
+
+
+    print("loaded_filtered_text[:5]: ", loaded_filtered_text[:5])
+
+    # checking if loaded filtered text matches filtered text computed
+    if loaded_filtered_text == cleaned_dataset[key]['text']:
+        print("The lists are the same.")
+    else:
+        print("The lists are different.")
 
 
 
